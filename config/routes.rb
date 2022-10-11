c204a3598788446b004ee4b37c2b0e944bbbb859
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-10-11 08:07:16 +0000

    [Finish]4-7

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root to: "homes#top"
 
-get "homes/about"
+get "homes/about" => "homes#about", as: :about
 end
