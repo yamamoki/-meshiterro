06de0c5f93d73a60b4139dbeefe94a8f8985eb58
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-10-11 05:35:22 +0000

    [add]4-6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parametera, if: :devise_controller?
+  
+  protected
+  
+  def configure_permitted_parametera
+    devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
+  end
 end
