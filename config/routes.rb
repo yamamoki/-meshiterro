b5460c5fe93246fe7a49fa47d831259c78e1d938
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-10-12 14:32:48 +0000

    [Finish]4-16

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 resources :post_images, only: [:new, :create, :index, :show, :destroy]
-resources :users, only: [:show, :edit]
+resources :users, only: [:show, :edit, :update]
 get "homes/about" => "homes#about", as: :about
 end
