8b085c92aca8721eb5fcb99eece845bceba9b6bd
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-10-13 05:51:42 +0000

    [Finish]4-19

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,7 +3,8 @@ class PostImage < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   has_many :post_comments, dependent: :destroy
-  
+  has_many :favorites, dependent: :destroy
+
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
@@ -11,5 +12,9 @@ class PostImage < ApplicationRecord
     end
     image
   end
+
+def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+end
 
 end
