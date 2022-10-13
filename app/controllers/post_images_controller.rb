b9019be447a8776e7d2e9f52a2f11ed1251b6a94
yamamoki <112685611+yamamoki@users.noreply.github.com>
2022-10-13 07:29:02 +0000

    [Finish]4-20

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -2,14 +2,18 @@ class PostImagesController < ApplicationController
   def new
     @post_image = PostImage.new
   end
-  
+
   # 投稿データの保存
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
-    @post_image.save
+    if@post_image.save
     redirect_to post_images_path
-  end
+    else
+      render :new
+    end
+   end
+
 
   def index
     @post_images = PostImage.all
@@ -19,18 +23,18 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.find(params[:id])
     @post_comment = PostComment.new
   end
-  
+
   def destroy
     @post_image = PostImage.find(params[:id])
     @post_image.destroy
     redirect_to '/post_images'
   end
-  
+
   # 投稿データのストロングパラメータ
   private
 
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
-  
+
 end
